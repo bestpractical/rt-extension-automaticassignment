1b11bdae03e52cbd4937379e2980435565e2d098
Shawn M Moore <shawn@bestpractical.com>
2016-07-21 13:35:36 +0000

    Round Robin chooser

---
+++ lib/RT/Extension/AutomaticAssignment/Chooser/RoundRobin.pm
@@ -0,0 +1,31 @@
+package RT::Extension::AutomaticAssignment::Chooser::RoundRobin;
+use strict;
+use warnings;
+use base 'RT::Extension::AutomaticAssignment::Chooser';
+use List::Util 'reduce';
+
+sub ChooseOwnerForTicket {
+    my $class  = shift;
+    my $ticket = shift;
+    my $users  = shift;
+    my $config = shift;
+
+    my $queue = $ticket->Queue;
+    my $attr = 'AutomaticAssignment-RoundRobin-Queue' . $queue;
+
+    # find the user whose last round-robin automatic assignment in this queue
+    # was the longest time ago
+    my $owner = reduce {
+        ($a->FirstAttribute($attr)||0) < ($b->FirstAttribute($attr)||0) ? $a : $b
+    } @$users;
+
+    if ($owner) {
+        $owner->SetAttribute(Name => $attr, Content => time);
+    }
+
+    return $owner;
+}
+
+1;
+
+
