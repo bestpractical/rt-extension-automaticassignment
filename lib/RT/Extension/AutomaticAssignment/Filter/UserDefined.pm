a6bd4b152a7f676baf36035666c43507ad3b5cae
Shawn M Moore <shawn@bestpractical.com>
2017-04-13 21:30:42 +0000

    Add user-defined choosers and filters

---
+++ lib/RT/Extension/AutomaticAssignment/Filter/UserDefined.pm
@@ -0,0 +1,38 @@
+package RT::Extension::AutomaticAssignment::Filter::UserDefined;
+use strict;
+use warnings;
+use base 'RT::Extension::AutomaticAssignment::Filter';
+
+sub FilterOwnersForTicket {
+    my $class   = shift;
+    my $Ticket  = shift;
+    my @Users   = @{ shift(@_) };
+    my $Config  = shift;
+    my $Context = shift;
+
+    my @matches;
+
+    for my $User (@Users) {
+        push @matches, $User if eval $Config->{code};
+    }
+
+    return \@matches;
+}
+
+sub Description { "User Defined" }
+
+sub FiltersUsersArray {
+    return 1;
+}
+
+sub CanonicalizeConfig {
+    my $class = shift;
+    my $input = shift;
+
+    my $code = $input->{code};
+
+    return { code => $code };
+}
+
+1;
+
